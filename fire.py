a220c1acb48fa513827a5773699e2ac13fa3a691
Eudson <eudson.duraes@gmail.com>
2023-08-12 21:00:32 -0300

    Adicionando pyproject toml

--- fire.py
+++ fire.py
@@ -4,30 +4,31 @@ Estudando o algoritmo Doom fire
 Desempenho inicial: 30 fps de media
 Desempenho com numpay + numba: 50 fps de media
 """
-import pygame as pg
-import numpy as np
-from pygame import gfxdraw
 from random import randint
+
+import numpy as np
+import pygame as pg
 from numba import njit
+from pygame import gfxdraw
+
 
 class DoomFire:
-
     STEPS_BETWEEN_COLORS = 9
-    COLORS = ['black','red','orange','yellow','white']
+    COLORS = ["black", "red", "orange", "yellow", "white"]
     PIXEL_SIZE = 4
-  
-    def __init__(self,app):
+
+    def __init__(self, app):
         self.app = app
 
         self.FIRE_WIDTH = self.app.WIDTH // self.PIXEL_SIZE
-        self.FIRE_HEIGHT = self.app.HEIGHT  // self.PIXEL_SIZE
+        self.FIRE_HEIGHT = self.app.HEIGHT // self.PIXEL_SIZE
 
         self.palette = self.get_palette()
         self.fire_array = self.get_fire_array()
 
     @staticmethod
     @njit(fastmath=True)
-    def do_fire(array,fire_width,fire_height):
+    def do_fire(array, fire_width, fire_height):
         """
         Atualiza o fire_array com novas cores do frame atual
         """
@@ -40,58 +41,73 @@ class DoomFire:
                 else:
                     array[y - 1][x] = 0
         return array
-    
+
     @staticmethod
     @njit()
     def calc_fire(array):
         """
-        Função geradora que foi isolada para poder acelerar com numba
+        Funcao geradora que foi isolada para poder acelerar com numba
         """
         for y, row in enumerate(array):
             for x, color_index in enumerate(row):
                 if color_index:
-                    yield x,y,color_index
+                    yield x, y, color_index
 
     def draw_fire(self):
         """
-        Função que pinta a tela de acordo com os valores do fire array.
+        Funcao que pinta a tela de acordo com os valores do fire array.
         """
-        for (x,y,color_index) in self.calc_fire(self.fire_array):  
-            gfxdraw.box(self.app.screen, (x * self.PIXEL_SIZE, y * self.PIXEL_SIZE, self.PIXEL_SIZE -1, self.PIXEL_SIZE -1), self.palette[color_index])
-    
+        for x, y, color_index in self.calc_fire(self.fire_array):
+            gfxdraw.box(
+                self.app.screen,
+                (
+                    x * self.PIXEL_SIZE,
+                    y * self.PIXEL_SIZE,
+                    self.PIXEL_SIZE - 1,
+                    self.PIXEL_SIZE - 1,
+                ),
+                self.palette[color_index],
+            )
+
     def get_fire_array(self):
         """
-        Retorna um array com os indices das cores. Atenção não retorna as cores no formato rgb
+        Retorna um array com os indices das cores. Atencao nao retorna as cores no formato rgb
         retorna uma lista com indexes dos valores da palleta 'COLORS'
         """
-        fire_array = [[0 for i in range(self.FIRE_WIDTH)] for j in range(self.FIRE_HEIGHT)]
+        fire_array = [
+            [0 for i in range(self.FIRE_WIDTH)] for j in range(self.FIRE_HEIGHT)
+        ]
         for i in range(self.FIRE_WIDTH):
             fire_array[self.FIRE_HEIGHT - 1][i] = len(self.palette) - 1
-        
+
         return np.array(fire_array)
-        
+
     def draw_pallete(self):
         size = 50
         for i, color in enumerate(self.palette):
-            pg.draw.rect(self.app.screen, color, (i * size, self.app.HEIGHT // 2, size -5, size - 5))
+            pg.draw.rect(
+                self.app.screen,
+                color,
+                (i * size, self.app.HEIGHT // 2, size - 5, size - 5),
+            )
 
     def get_palette(self):
         """
         Gera um gradiente de cores de acordo com as cores escolhida na palleta
         """
-        pallete = [(0,0,0)]
+        pallete = [(0, 0, 0)]
         for i, color in enumerate(self.COLORS[:-1]):
-            c1,c2 = color, self.COLORS[i+1]
+            c1, c2 = color, self.COLORS[i + 1]
             for step in range(self.STEPS_BETWEEN_COLORS):
-                c = pg.Color(c1).lerp(c2,(step + 0.5) / self.STEPS_BETWEEN_COLORS)
+                c = pg.Color(c1).lerp(c2, (step + 0.5) / self.STEPS_BETWEEN_COLORS)
                 pallete.append(c)
         return pallete
 
     def update(self):
-        self.fire_array = self.do_fire(self.fire_array,self.FIRE_WIDTH,self.FIRE_HEIGHT)
-    
+        self.fire_array = self.do_fire(
+            self.fire_array, self.FIRE_WIDTH, self.FIRE_HEIGHT
+        )
+
     def draw(self):
-        #self.draw_pallete()
+        # self.draw_pallete()
         self.draw_fire()
-
-
--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 """
 Arquivo principal, aqui fica a classe jogo
 """
-import pygame as pg
 import sys
+
+import pygame as pg
 
 from fire import DoomFire
 
 
 class Game:
-    
     WIDTH, HEIGHT = 600, 600
-    WIN_SIZE = WIDTH, HEIGHT 
-    FPS = 30
+    WIN_SIZE = WIDTH, HEIGHT
+    FPS = 60
 
     def __init__(self):
         self.screen = pg.display.set_mode(size=self.WIN_SIZE)
         self.clock = pg.time.Clock()
         self.doom_fire = DoomFire(self)
-    
+
     def update(self):
         self.doom_fire.update()
         self.clock.tick(self.FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
-    
+
     def draw(self):
-        self.screen.fill('black')
+        self.screen.fill("black")
         self.doom_fire.draw()
         pg.display.flip()
-    
+
     def run(self):
         while True:
             for event in pg.event.get():
@@ -37,6 +37,7 @@ class Game:
             self.update()
             self.draw()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = Game()
     app.run()
